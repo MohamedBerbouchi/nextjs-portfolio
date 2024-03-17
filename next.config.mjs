a721a26b140d01a9656af0412fcdf4c96ff80b99
MohamedBerbouchi <82718864+MohamedBerbouchi@users.noreply.github.com>
2024-03-17 14:52:56 +0000

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com", 'https://nextjs-portfolio-90suiwvlu-mohamedberbouchi.vercel.app/'],
+    domains: ["images.unsplash.com", "plus.unsplash.com", 'nextjs-portfolio-90suiwvlu-mohamedberbouchi.vercel.app/'],
   },
 };
 
