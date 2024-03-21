f928c9438a490ef28c4cbc3ad5b1ca502794a136
MohamedBerbouchi <82718864+MohamedBerbouchi@users.noreply.github.com>
2024-03-21 15:56:45 +0000

    edit

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -13,14 +13,14 @@ function AboutMe() {
             <div className="max-w-[600px] flex flex-col gap-3 mt-10 text-center md:text-left">
                 <p className="text-3xl font-semibold">About me</p>
                 <p className=" font-thin ">
-                🚀 Passion for React and Next.js I love building cool stuff with React and Next.js. Whether it's making interactive websites or dynamic web apps, I'm all in!
+                🚀 Passion for React and Next.js I love building cool stuff with React and Next.js. Whether it&apos;s making interactive websites or dynamic web apps, I&apos;m all in!
 <br />
-💼 Getting the Backend Right with Express: I'm learning the ropes of backend development with Express. Creating APIs and server-side features? Count me in!
+💼 Getting the Backend Right with Express: I&apos;m learning the ropes of backend development with Express. Creating APIs and server-side features? Count me in!
 <br />
-✨ Growing Every Day: I'm on a journey of continuous learning and improvement. Let's grow together!
+✨ Growing Every Day: I&apos;m on a journey of continuous learning and improvement. Let&apos;s grow together!
 
 <br />
-💡 Eager to Learn and Collaborate: I'm a team player who loves learning new things.
+💡 Eager to Learn and Collaborate: I&apos;m a team player who loves learning new things.
                </p>
             </div>
         </section>
