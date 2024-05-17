f1a721c50425a1e045b7bdc22ebe829ea0e3c32f
MohamedBerbouchi <82718864+MohamedBerbouchi@users.noreply.github.com>
2024-05-18 00:47:18 +0100

    add 2 project

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -85,6 +85,24 @@ export const PROJECTS: TProjectProps[] = [
     tags:['Laravel','php', 'MySQL']
 
   },
+  {
+    title: "Real Estat App",
+    desc: "The portfolio is crafted with a mobile-first approach, ensuring a seamless experience across various devices and screen sizes.",
+    image: "/images/project-3.png",
+    github_link: "https://github.com/MohamedBerbouchi/nextjs-portfolio",
+    demo_link: "https://nextjs-portfolio-two-ashen.vercel.app/",
+    tags:['React','Express', 'sass', 'MongoDb']
+
+  },
+  {
+    title: "E-commerce website",
+    desc: "The portfolio is crafted with a mobile-first approach, ensuring a seamless experience across various devices and screen sizes.",
+    image: "/images/project-4.svg",
+    github_link: "https://github.com/MohamedBerbouchi/nextjs-portfolio",
+    demo_link: "https://nextjs-portfolio-two-ashen.vercel.app/",
+    tags:['php', 'MySQL']
+
+  },
 ];
 
 
